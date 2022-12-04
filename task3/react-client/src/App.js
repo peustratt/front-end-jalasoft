a118c45a7fd3852311668eac817bb72dac167596
pedro-ivo <peustratt@gmail.com>
2022-12-04 20:05:47 -0300

    Fetching predictions from API

--- task3/react-client/src/App.js
+++ task3/react-client/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import Prediction from "./components/Prediction/Prediction";
 
 function App() {
   const [data, setData] = useState("waiting for the useEffect to run!");
+  const [predictions, setPredictions] = useState(() => []);
 
   useEffect(() => {
     const getData = async () => {
@@ -16,13 +18,51 @@ function App() {
       }
     };
 
+    const getPredictions = () => {
+      fetch(
+        "https://football-prediction-api.p.rapidapi.com/api/v2/predictions?market=classic&iso_date=2018-12-01&federation=UEFA",
+        {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key":
+              "b2bc95e5a1mshee74a0437a26fefp1ac41djsn80e1031d3c1e",
+            "X-RapidAPI-Host": "football-prediction-api.p.rapidapi.com",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          console.log(json.data);
+          setPredictions(json.data);
+        });
+    };
+
     getData();
-  });
+    getPredictions();
+  }, []);
+
+  const predictionsEl = predictions?.map((pred) => (
+    <Prediction
+      key={pred.id}
+      awayTeam={pred.away_team}
+      homeTeam={pred.home_team}
+      result={pred.result}
+    />
+  ));
 
   return (
     <div className="App">
-      <h3>Backend /api/test route content</h3>
+      <h1>Task3 Frontend class</h1>
+      <h2>Part 1</h2>
+      <h3>Backend route "/api/test"</h3>
+      <span>Fetched message: </span>
       <span>{data}</span>
+
+      <h2>Part 2</h2>
+      <h3>Predictions API</h3>
+      <div className="predictions">
+        {predictions && predictionsEl.slice(0, 10)}
+      </div>
     </div>
   );
 }
